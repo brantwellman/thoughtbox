616092371006e15d472240df9410456dea98be23
Brant Wellman <BrantWellman@gmail.com>
2016-04-18 10:26:40 -0600

    Add sad path tests
    Test for user cannot sign up with an already used email
    User model now validates email uniqueness and presence
    Test for password and pass confirmation must match

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    email "MyString"
-    password_digest "MyString"
+    email "brant@email.com"
+    password_digest "password"
   end
 end
--- test/integration/unauthenticated_user_test.rb
+++ test/integration/unauthenticated_user_test.rb
@@ -20,4 +20,32 @@ class UnauthenticatedUserTest < ActionDispatch::IntegrationTest
 
     assert page.has_content?("All of the links!")
   end
+
+  test "user cant sign up with an email that is already used" do
+    create(:user)
+
+    visit root_path
+    click_on "Sign Up"
+    fill_in "Email", with: "brant@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Submit"
+
+    assert page.has_content?("Email has already been taken")
+  end
+
+  test "user cant sign up with a password and confirmation that don't match" do
+    create(:user)
+
+    visit root_path
+    click_on "Sign Up"
+    fill_in "Email", with: "brant@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password2"
+    click_on "Submit"
+
+    assert page.has_content?("Password confirmation doesn't match Password")
+  end
+
+  
 end
