d286244473869149ead4aa87c17f5cf09b2bcb6e
Brant Wellman <BrantWellman@gmail.com>
2016-04-18 12:42:22 -0600

    Add default false value to read column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418173850) do
+ActiveRecord::Schema.define(version: 20160418184112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(version: 20160418173850) do
   create_table "links", force: :cascade do |t|
     t.string   "url"
     t.string   "title"
-    t.boolean  "read"
+    t.boolean  "read",       default: false
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   add_index "links", ["user_id"], name: "index_links_on_user_id", using: :btree
