3b17e6e297cbe61104fb32c4aeb82a06c09af479
Brant Wellman <BrantWellman@gmail.com>
2016-04-18 12:56:16 -0600

    Add unhappy path tests for link submission
    User sees error message for invalid url
    User can view only own links from index page

--- test/integration/link_submission_viewing_test.rb
+++ test/integration/link_submission_viewing_test.rb
@@ -14,4 +14,28 @@ class UserCanSubmitandViewLinksTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Facebook")
     assert page.has_content?("https://www.facebook.com")
   end
+
+  test "user sees error message for an invalid url submission" do
+    user = User.create(email: "me@email.com", password: "password")
+    login(user)
+
+    assert_equal root_path, current_path
+
+    fill_in "Title", with: "Facebook"
+    fill_in "Url", with: "hs:/w.facebook.com"
+    click_on "Submit"
+
+    assert page.has_content?("Url is not a valid URL")
+  end
+
+  test "user sees only his own links" do
+    Link.create(title: "Facebook", url: "https://www.facebook.com")
+
+    user = User.create(email: "me@email.com", password: "password")
+    user.links << Link.create(title: "MyPlace", url: "https://www.myplace.com")
+    login(user)
+
+    assert page.has_content?("MyPlace")
+    refute page.has_content?("Facebook")
+  end
 end
