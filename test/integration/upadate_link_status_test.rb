7972481295d78815481f49f6426e7a9bdce07083
Brant Wellman <BrantWellman@gmail.com>
2016-04-18 14:25:02 -0600

    Add style to read links
    Read links are now grayed out with strikethrough
    Add test for clicking on Mark as Unread link

--- test/integration/upadate_link_status_test.rb
+++ test/integration/upadate_link_status_test.rb
@@ -13,4 +13,17 @@ class UserUpdatesLinkStatusTest < ActionDispatch::IntegrationTest
 
     assert page.has_content?("Mark as Unread")
   end
+
+  test "User clicks on Mark as Unread link and observes change" do
+    user = User.create(email: "me@email.com", password: "password")
+    user.links << Link.create(title: "MyPlace", url: "https://www.myplace.com", read: true)
+    login(user)
+
+    assert page.has_content?("Mark as Unread")
+    refute page.has_content?("Mark as Read")
+
+    click_on("Mark as Unread")
+
+    assert page.has_content?("Mark as Read")
+  end
 end
