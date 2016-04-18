ab69b56d30aa5b1811691fe703fed9ea2308ee1a
Brant Wellman <BrantWellman@gmail.com>
2016-04-18 10:13:52 -0600

    Unauthenticated user can signup
    Create user model w/ has_secure_password
    Add bcrypt gem
    Add create user method
    Add new user form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "links#index"
 
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create]
 
   get "/enter", to: "sessions#enter"
   get "/login", to: "sessions#new"
--- test/integration/unauthenticated_user_test.rb
+++ test/integration/unauthenticated_user_test.rb
@@ -8,4 +8,16 @@ class UnauthenticatedUserTest < ActionDispatch::IntegrationTest
     assert page.has_link?("Login")
     assert page.has_content?("Sign Up")
   end
+
+  test "unauthenticated user can sign up by clicking the signup button" do
+    visit root_path
+    click_on "Sign Up"
+
+    fill_in "Email", with: "brant@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Submit"
+
+    assert page.has_content?("All of the links!")
+  end
 end
